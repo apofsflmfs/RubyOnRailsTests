42549e2b03750b462772b45b0c873e1b9ebcf730
Javibz <ramonjavi8@gmail.com>
2018-11-05 20:30:25 +0100

    Fonction edit presque fonctionnelle, mais les tests passent

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,16 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
   private
 
   def user_params
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -30,4 +30,36 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
   end
-end  
+
+  test "show: show link to profile only to logged users" do
+    post users_path, params: { user: { first_name: @user.first_name, last_name: @user.last_name, mail: @user.email, password: 'foobar', password_confirmation: "foobar" } }
+    post login_path, params: { session: { email:@user.email, password: 'foobar' } }
+    get user_path(@user.id)
+  end
+
+  test "show: show users info when logged in" do
+    post login_path, params: { session: { email:@user.email, password: 'foobar' } }
+    get user_path(@user.id)
+  end
+
+  test "show: show page only to logged users" do
+    post users_path, params: { user: { first_name: @user.first_name, last_name: @user.last_name, mail: @user.email, password: 'foobar', password_confirmation: "foobar" } }
+    post login_path, params: { session: { email:@user.email, password: 'foobar' } }
+    get user_path(@user.id)
+  end
+
+  test "show: should send back to login page if not logged in" do
+    get user_path(@user.id)
+    follow_redirect!
+    assert_template 'sessions/new'
+  end
+
+  test "edit: edit user's profile" do
+    post login_path, params: { session: { email:@user.email, password: 'foobar' } }
+    get user_path(@user.id)
+    get edit_user_path
+    put user_path, params: {user: {first_name: @user.first_name, last_name: @user.last_name, email: @user.email}}
+    get user_path(@user.id)
+  end
+
+end
